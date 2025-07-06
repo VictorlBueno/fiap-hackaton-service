f559aa5de43fbb4e719d08d2b842e4a11a36fbda
Victor <victorlzbu@gmail.com>
2025-07-06 15:52:44 -0300

    feat: ecr terraform

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "this" {
+  name                 = "${var.project_name}-${var.environment}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_delete
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.this.repository_url
+} 
