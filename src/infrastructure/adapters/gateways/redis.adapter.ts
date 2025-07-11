f2805aaa35caaf7c1ae7fc0fc42f5410a932033a
Victor <victorlzbu@gmail.com>
2025-07-10 23:52:44 -0300

    fix redis secret

--- src/infrastructure/adapters/gateways/redis.adapter.ts
+++ src/infrastructure/adapters/gateways/redis.adapter.ts
@@ -11,6 +11,7 @@ export class RedisJobRepositoryAdapter implements JobRepositoryPort {
     this.redis = new Redis({
       host: process.env.REDIS_HOST || 'redis-service.redis.svc.cluster.local',
       port: parseInt(process.env.REDIS_PORT || '6379', 10),
+      password: process.env.REDIS_PASSWORD,
     });
   }
 
